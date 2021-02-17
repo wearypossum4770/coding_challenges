5d787535e1002c70199786f929aff0d409c55405
wearypossum4770 <wearypossum4770@yahoo.com>
2021-02-17 17:52:26 -0500

    complete rust addition challenge

---
+++ rust/src/addition.rs
@@ -0,0 +1,21 @@
+pub fn addition(a: i64, b: i64) -> i64 {
+    return a + b;
+}
+
+macro_rules! addition_tests {
+	($($name:ident: $value:expr,)*) => {
+		$(
+#[test]
+fn $name(){
+	let (a,b, expected) = $value;
+	assert_eq!(expected,addition(a,b) );
+})*
+	};
+}
+#[cfg(test)]
+addition_tests! {
+    addition_pos_5:(2,3,5),
+    addition_neg_9:(-3, -6, -9),
+    addition_pos_10:(7, 3, 10),
+    addition_pos_90:(88, 2, 90),
+}
---
+++ rust/src/main.rs
@@ -0,0 +1,7 @@
+use crate::addition::addition;
+mod addition;
+
+fn main() {
+let mut _array = vec![addition(2,3)];
+    println!("Hello, world!");
+}
