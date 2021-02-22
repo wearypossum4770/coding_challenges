a131f567da44e59d52eaa8c29dbd8bb64b761582
stephen smith <wearypossum4770@yahoo.com>
2021-02-22 16:09:38 -0500

    complete remove-array-duplicate challenge

---
+++ js/src/removeDuplicates.js
@@ -0,0 +1,9 @@
+export default function removeDuplicates(nums){
+    for (let index=0; index<nums.length;index++){
+        while(nums[index] === nums[index+1]){
+            nums.splice(index,1)
+        }
+
+    }
+    return nums
+}
---
+++ js/tests/removeDuplicates.test.js
@@ -0,0 +1,14 @@
+import removeDuplicates from '../src/easy/removeDuplicates.js'
+
+test.each(
+
+    [
+        [[1,1,2], [1,2]],
+       [ [0,0,1,1,1,2,2,3,3,4], [0,1,2,3,4]],
+       [[1,1,1,1], [1]]
+    ]
+)
+
+("duplicates removed", (data,expected)=>{
+    expect(removeDuplicates(data)).toStrictEqual(expected)
+})
