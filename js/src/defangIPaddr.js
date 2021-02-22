d2025850f0fbcec8db8e1a6e99b17cfea23d35a5
stephen smith <wearypossum4770@yahoo.com>
2021-02-22 16:08:28 -0500

    complete ip-address-defang challenge

---
+++ js/src/defangIPaddr.js
@@ -0,0 +1,7 @@
+export default function defangIPaddr(address){
+    let newAddress = ''
+    for (let index=0;index<address.length;index++){
+        address[index]!=='.'?newAddress+=address[index]:newAddress+=`[${address[index]}]`
+    }
+    return newAddress
+}
