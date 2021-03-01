c86be9d64f6c95e3b366de912a7f11f621617a1e
dragoomdoc <dragoomdoc@penguin>
2021-02-28 22:03:48 -0500

    feat: complete reverse-word in js

---
+++ js/tests/reverseWords.test.js
@@ -0,0 +1,8 @@
+import reverseWords from "../src/reverseWords.js"
+
+test("someting",()=>{
+	const expected="blue is sky the"
+	const func = reverseWords("the sky is blue")
+	expect(func).toStrictEqual(expected)
+	
+	})
