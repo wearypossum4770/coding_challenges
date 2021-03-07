decc078d3b85f800bc49162ccfd89151f95192a5
stephen smith <wearypossum4770@yahoo.com>
2021-03-06 20:12:14 -0500

    feat: add js sort nums desc function

---
+++ js/src/sortDescending.js
@@ -0,0 +1,10 @@
+export const sortDescending = (nums) => {
+  let array = [];
+  let len = nums.toString();
+  return len;
+  // .length
+  for (let index = 0; index < len; index++) {
+    array.push(nums);
+  }
+  return array;
+};
