89968c866d35724a2a84f724e4aa2bc032eeb214
stephen smith <wearypossum4770@yahoo.com>
2021-03-19 00:17:07 -0400

    feat(jscript): compelete name string challenge

---
+++ js/tests/nameString.test.js
@@ -0,0 +1,12 @@
+import nameString from '../src/nameString.js'
+
+test.each([
+    
+["Mubashir", "MubashirEdabit"],
+["Matt", "MattEdabit"],
+["javaScript", "javaScriptEdabit"],
+["Airforce", "AirforceEdabit"],
+])("concatenates the string", (testInput, testOutput)=> {
+    let func = nameString(testInput)
+    expect(func).toBe(testOutput)
+})
