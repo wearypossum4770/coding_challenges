ca1451173c7c9ed1f563172938a2de9158345b5a
stephen smith <wearypossum4770@yahoo.com>
2021-03-19 00:19:00 -0400

    feat(rust): complete alphabet soup challenge

--- rust/src/alphabet_soup.rs
+++ rust/src/alphabet_soup.rs
@@ -1,29 +1,28 @@
 pub fn alphabet_soup(input: &str) -> String {
-
-    let mut buf = String::with_capacity(input.len());
+    let mut vec = Vec::new();
     for c in input.chars() {
-       println!("{:?}", c);
-    //    to_ascii_lowercase
-       if c != ' ' {
-          buf.push(c);
-       }
+        if c != ' ' {
+            vec.push(c);
+        }
     }
-    buf.v.sort_unstable();
+    vec.sort();
+    let sorted: String = vec.into_iter().collect();
+    sorted
 }
 #[cfg(test)]
 mod tests {
     use super::alphabet_soup;
     #[test]
     fn test_alphabet_soup() {
-        assert_eq!(alphabet_soup("hello"),"ehllo");
-        assert_eq!(alphabet_soup("edabit"),"abdeit");
-        assert_eq!(alphabet_soup("hacker"),"acehkr");
-        assert_eq!(alphabet_soup("geek"),"eegk");
-        assert_eq!(alphabet_soup("javascript"),"aacijprstv");
-        assert_eq!(alphabet_soup("credibility"),"bcdeiiilrty");
-        assert_eq!(alphabet_soup("apostrophe"),"aehoopprst");
-        assert_eq!(alphabet_soup("determination"),"adeeiimnnortt");
-        assert_eq!(alphabet_soup("win"),"inw");
-        assert_eq!(alphabet_soup("synthesis"),"ehinsssty");
+        assert_eq!(alphabet_soup("hello"), "ehllo");
+        assert_eq!(alphabet_soup("edabit"), "abdeit");
+        assert_eq!(alphabet_soup("hacker"), "acehkr");
+        assert_eq!(alphabet_soup("geek"), "eegk");
+        assert_eq!(alphabet_soup("javascript"), "aacijprstv");
+        assert_eq!(alphabet_soup("credibility"), "bcdeiiilrty");
+        assert_eq!(alphabet_soup("apostrophe"), "aehoopprst");
+        assert_eq!(alphabet_soup("determination"), "adeeiimnnortt");
+        assert_eq!(alphabet_soup("win"), "inw");
+        assert_eq!(alphabet_soup("synthesis"), "ehinsssty");
     }
 }
