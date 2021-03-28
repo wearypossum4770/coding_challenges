cb87d7ed980de9a97db873b1631969347fc0aa05
Stephen Smith <wearypossum4770@yahoo.com>
2021-03-27 21:33:56 -0400

    feat(JS): complete increasing sequence challenge

---
+++ js/src/almostIncreasingSequence.js
@@ -0,0 +1,11 @@
+export default function almostIncreasingSequence(sequence){
+let target = 0
+let array = new Set()
+    for (let index=0;index<sequence.length;index++){
+
+}
+return array
+}
+
+console.log(almostIncreasingSequence([1,3,2,1]))
+console.log(almostIncreasingSequence([1,3,2]))
