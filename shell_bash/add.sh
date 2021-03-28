d309eebc1c74ced2674cd5d0181a64d4e77edebb
Stephen Smith <wearypossum4770@yahoo.com>
2021-03-27 21:44:35 -0400

    feat(bash): completed add shell challenge

--- shell_bash/add.sh
+++ shell_bash/add.sh
@@ -2,22 +2,20 @@
 
 read -a NUMBERS
 COUNTER=0
-for number in "${NUMBERS[@]}"
-do
-if [ $number -gt 0 ]
-then
-    COUNTER=$(( $COUNTER + $number ))
-else 
-    COUNTER=$(( $COUNTER - $number ))
-fi
+for number in "${NUMBERS[@]}"; do
+  if [ $number -gt 0 ]; then
+    COUNTER=$(($COUNTER + $number))
+  else
+    COUNTER=$(($COUNTER - $number))
+  fi
 done
-   echo $COUNTER
+echo $COUNTER
 # array=($(echo $string| tr " " "\n"))
-# for number in ; do 
+# for number in ; do
 # if  (($number -gt 0 ));
-# then 
+# then
 # COUNTER = COUNTER+number
-# else 
+# else
 # COUNTER=COUNTER-number
 # fi
 # echo $COUNTER
