00fdb0a03f31e69a75fd6885970cc1cee8b10a84
Stephen Smith <wearypossum4770@yahoo.com>
2021-03-27 21:55:03 -0400

    fix(rust): sync reduce array

--- rust/src/reduce_array.rs
+++ rust/src/reduce_array.rs
@@ -12,7 +12,6 @@ pub fn reduce_array(array: &str) -> i32 {
 #[cfg(test)]
 mod tests {
     use super::reduce_array;
-
     #[test]
     fn test_reduce_array() {
         assert_eq!(reduce_array("1 2 3 4 10 11"), 31);
