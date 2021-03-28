32359484f33e97f933b8046eddc24b874250439f
Stephen Smith <wearypossum4770@yahoo.com>
2021-03-27 21:41:11 -0400

    feat(JS): complete baseball point challenge

--- js/src/baseballPoints.js
+++ js/src/baseballPoints.js
@@ -1,3 +1,3 @@
-export default function baseballPoints(twoPointers, threePointers){
-    return twoPointers*2+threePointers*3
+export default function baseballPoints(twoPointers, threePointers) {
+  return twoPointers * 2 + threePointers * 3;
 }
