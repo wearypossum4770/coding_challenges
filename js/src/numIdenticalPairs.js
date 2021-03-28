7201d034c44853a79fd866dc3fb375ef4e1a46e8
Stephen Smith <wearypossum4770@yahoo.com>
2021-03-27 21:47:51 -0400

    feat(JS): sync identical pair challenge

---
+++ js/src/numIdenticalPairs.js
@@ -0,0 +1,10 @@
+export default function numIdenticalPairs(nums){
+    let count = 0
+    for (let index=0; index<nums.length; index++){
+        
+    }
+
+
+}
+
+console.log(numIdenticalPairs( [1,2,3,1,1,3]))
