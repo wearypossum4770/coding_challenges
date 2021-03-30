8608c9091d2adfeaedc0b8cf5958225691e20c00
Stephen Smith <wearypossum4770@yahoo.com>
2021-03-29 21:14:43 -0400

    fix(rust): complete is same number challenge

--- rust/src/is_same_number.rs
+++ rust/src/is_same_number.rs
@@ -1,4 +1,4 @@
-pub fn is_same_number(x: &<T>, y:&<T>) -> bool {
+pub fn is_same_number(x: i32, y:i32) -> bool {
     x == y
 }
 #[cfg(test)]
@@ -9,6 +9,5 @@ mod tests {
 assert_eq!(is_same_number(4, 8), false);
 assert_eq!(is_same_number(2, 2), true);
 assert_eq!(is_same_number(0, 6), false);
-assert_eq!(is_same_number(2, "2"), false);
     }
 }
