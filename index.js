fc58cb681f561651a94fbc30d2958778fd0c1eae
stephen smith ecs <wearypossum4770@yahoo.com>
2022-07-13 20:31:02 -0400

    try to run actions

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-import { readFile } from 'fs'
-import * as core from '@actions/core';
+import { readFile } from "fs";
+import * as core from "@actions/core";
 
-
-const isAuthorized = ({username}) => {
-  console.log(username)
-}
+const isAuthorized = ({ username }) => {
+  console.log(username);
+};
 const hasPermissions = ({ permissions }) => {
-  console.log(permissions)
-}
+  console.log(permissions);
+};
 
 const isAuthenticated = (user) => {
-console.log(user)
-}
-readFile('./code_approvers.json', 'utf-8', (err, data) => {
-  if (err) return err
-  isAuthenticated.apply(null, JSON.parse(data))
-})
+  console.log(user);
+};
+readFile("./code_approvers.json", "utf-8", (err, data) => {
+  if (err) return err;
+  isAuthenticated.apply(null, JSON.parse(data));
+});
